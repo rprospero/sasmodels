2b9287c5a1b8d1c9df7ac3384f6734384aa3fa46
Adam Washington <adam.washington@stfc.ac.uk>
2025-05-20 17:51:02 +0100

    Get the requirements to build undex nix (but probably nowhere else)

---
+++ flake.nix
@@ -0,0 +1,103 @@
+{
+  # inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "nixpkgs/nixos-24.11";
+  inputs.outdated.url = "nixpkgs/nixos-24.05";
+
+  inputs.pyproject-nix.url = "github:pyproject-nix/pyproject.nix";
+  inputs.pyproject-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+  inputs.hatch-sphinx.url = "github:llimeht/hatch-sphinx?ref=v0.0.3";
+  inputs.hatch-sphinx.flake = false;
+
+  inputs.siphash24.url = "github:dnicolodi/python-siphash24?ref=v1.7";
+  inputs.siphash24.flake = false;
+
+  inputs.columnize.url = "github:rocky/pycolumnize?ref=3.11";
+  inputs.columnize.flake = false;
+
+  inputs.tccbox.url = "github:metab0t/tccbox";
+  inputs.tccbox.flake = false;
+
+  outputs = { self, nixpkgs, outdated, pyproject-nix, hatch-sphinx, siphash24
+    , columnize, tccbox, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        past = import outdated { inherit system; };
+
+        project =
+          pyproject-nix.lib.project.loadPyproject { projectRoot = ./.; };
+        python = pkgs.python312.override {
+          packageOverrides = self: super: {
+            # hath-sphinx = self.packages.${system}.ruff;
+            hatch-sphinx = python.pkgs.buildPythonPackage
+              ((pyproject-nix.lib.project.loadPyproject {
+                projectRoot = hatch-sphinx;
+              }).renderers.buildPythonPackage { inherit python; } // {
+                version = "v0.0.3";
+              });
+
+            columnize = python.pkgs.buildPythonPackage {
+              pname = "pycolumnize";
+              version = "3.11";
+              src = columnize;
+            };
+
+            siphash24 = let
+              attrs = (pyproject-nix.lib.project.loadPyproject {
+                projectRoot = siphash24;
+              }).renderers.buildPythonPackage { inherit python; };
+            in python.pkgs.buildPythonPackage (attrs // { version = "1.7"; });
+
+            # Not actually tcc box, but we do not NEED tccbox, plus tccbox is *not* hermetic
+            tccbox = python.pkgs.buildPythonPackage {
+              pname = "pycolumnize";
+              version = "3.11";
+              src = columnize;
+            };
+            # tccbox = let
+            #   attrs = (pyproject-nix.lib.project.loadPyproject {
+            #     projectRoot = tccbox;
+            #   }).renderers.buildPythonPackage { inherit python; };
+            # in python.pkgs.buildPythonPackage (attrs // {
+            #   pname = "tccbox";
+            #   version = "0.0";
+            # });
+          };
+        };
+
+      in {
+
+        # Build our package using `buildPythonPackage
+        packages.default = let
+          # Returns an attribute set that can be passed to `buildPythonPackage`.
+          attrs = project.renderers.buildPythonPackage { inherit python; };
+          # Pass attributes to buildPythonPackage.
+          # Here is a good spot to add on any missing or custom attributes.
+        in python.pkgs.buildPythonPackage (attrs // {
+          version = "v0.9.0";
+          env.CUSTOM_ENVVAR = "hello";
+          nativeBuildInputs = with pkgs; [ git ];
+        });
+
+        devShells.default = let
+          # Returns a function that can be passed to `python.withPackages`
+          arg = project.renderers.withPackages { inherit python; };
+
+          # Returns a wrapped environment (virtualenv like) with all our packages
+          pythonEnv = python.withPackages arg;
+
+          # Create a devShell like normal.
+        in pkgs.mkShell {
+          packages = [
+            pythonEnv
+            past.hdfview
+            pkgs.ruff
+            pkgs.python312Packages.python-lsp-server
+            pkgs.python312Packages.python-lsp-ruff
+            pkgs.python312Packages.pylsp-mypy
+            pkgs.python312Packages.pylsp-rope
+          ];
+        };
+      });
+}
